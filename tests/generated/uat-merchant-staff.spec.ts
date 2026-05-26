6af59a3fe9e31c4c890b964d9a42828c7e68daa8
Anand Bala <anandbala@Anands-MacBook-Air.local>
2026-05-26 09:49:35 +0800

    added pages to increase abstraction

--- tests/generated/uat-merchant-staff.spec.ts
+++ tests/generated/uat-merchant-staff.spec.ts
@@ -3,9 +3,63 @@ import * as flows from '../flows';
 
 test.describe('UAT matrix role: Merchant Staff', () => {
 
+  test('Valid credentials - login succeeds [Login | authenticate]', async ({ page, context }) => {
+    // Role: Merchant Staff
+    // Feature/Module: Login
+    // Operation: authenticate
+    // Expected Behavior: Redirect to dashboard
+
+    await flows.loginAs(page, 'MERCHANT_STAFF');
+  });
+
+  test('Invalid credentials - login fails [Login | authenticate]', async ({ page, context }) => {
+    // Role: Merchant Staff
+    // Feature/Module: Login
+    // Operation: authenticate
+    // Expected Behavior: Show error message
+
+    await flows.loginWithInvalidCredentials(page, context, 'MERCHANT_STAFF');
+  });
+
   test.describe('Feature tests', () => {
     test.beforeEach(async ({ page }) => {
       await flows.loginAs(page, 'MERCHANT_STAFF');
+    });
+
+    test('Create ticket - Success [Support | create]', async ({ page, context }) => {
+      // Role: Merchant Staff
+      // Feature/Module: Support
+      // Operation: create
+      // Expected Behavior: Show success message
+
+      await flows.createTicketSuccess(page);
+    });
+
+    test('Empty ticket subject - creation fails [Support | create]', async ({ page, context }) => {
+      // Role: Merchant Staff
+      // Feature/Module: Support
+      // Operation: create
+      // Expected Behavior: Show error message
+
+      await flows.createTicketEmptySubject(page);
+    });
+
+    test('Empty ticket description - creation fails [Support | create]', async ({ page, context }) => {
+      // Role: Merchant Staff
+      // Feature/Module: Support
+      // Operation: create
+      // Expected Behavior: Show error message
+
+      await flows.createTicketEmptyDescription(page);
+    });
+
+    test('Lengthy ticket subject - creation fails [Support | create]', async ({ page, context }) => {
+      // Role: Merchant Staff
+      // Feature/Module: Support
+      // Operation: create
+      // Expected Behavior: Show error message
+
+      await flows.createTicketLongSubject(page);
     });
 
     test('Merchant/Branch Staffs option - does not exist [User Management | unable to create]', async ({ page, context }) => {
